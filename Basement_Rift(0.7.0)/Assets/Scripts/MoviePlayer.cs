78c9ffc65f203cf2370a1a6592451d4f43db2762
DaveCude <Mattcollins.earthenvr@gmail.com>
2015-11-06 22:27:54 -0500

    Project Imported

---
+++ Basement_Rift(0.7.0)/Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+
+
