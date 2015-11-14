8aed4889885edd49932a1b90bd85b21725e23dce
Infiniteye25 <infiniteye25@gmail.com>
2015-11-14 01:37:56 -0500

    FirstUpdate
    Added Oculus Audio SDK
    New Video Texture/New Audio
    Adjusted Wall Color
    BaseBoards Added
    DoorHandle Added
    TubeTVMesh/Textures Added
    StudyDeskHandles
    ReCenter Command (Space)

---
+++ Basement_Rift(0.7.0)/Assets/Scripts/RiftRecenter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class RiftRecenter : MonoBehaviour 
+{
+	
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown("space"))
+			InputTracking.Recenter();
+	}
+}
